96ae3239c4cd005104f96ffd8df45e3be4850d5e
SamA21 <Samaallen@gmail.com>
2021-12-04 10:04:52 +0000

    Day 3 - part 1

---
+++ AdventOfCode2021/Day3/ParseBinary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public class ParseBinary
+    {
+        public (string gamma, string epsilon) GetGammaAndEpsilon(List<string> binaryStrings)
+        {
+            string gammaString = string.Empty;
+            string epsilonString = string.Empty;
+            int totalCount = binaryStrings.Count;
+            string first = binaryStrings.First();
+            int index = 0;
+            foreach (char text in first)
+            {
+                int oneCount = MostCommonCount(binaryStrings, index);
+                if (oneCount > totalCount / 2)
+                {
+                    gammaString += "1";
+                    epsilonString += "0";
+                }
+                else
+                {
+                    gammaString += "0";
+                    epsilonString += "1";
+                }
+                index++;
+            }
+
+            return (gammaString, epsilonString);
+        }
+
+        private int MostCommonCount(List<string> binaryStrings, int index)
+        {
+            int oneCount = 0;
+            foreach (var binary in binaryStrings)
+            {
+                var test = binary[index];
+                if (test == '1')
+                    oneCount++;
+            }
+            return oneCount;
+        }
+    }
+}
---
+++ AdventOfCode2021/Day3/Program.cs
@@ -0,0 +1,14 @@
+﻿using AdventHelper;
+using Day3;
+
+AdventTextReader reader = new AdventTextReader();
+string inputLocation = $"data.txt";
+var binaryList = reader.GetListFromFile(inputLocation);
+ParseBinary parseBinary = new ParseBinary();
+(string gamma, string epsilon) = parseBinary.GetGammaAndEpsilon(binaryList);
+Console.WriteLine(gamma);
+Console.WriteLine(epsilon);
+int gammaNumber = Convert.ToInt32(gamma, 2);
+int epsilonNumber = Convert.ToInt32(epsilon, 2);
+int powerRate = gammaNumber * epsilonNumber;
+Console.WriteLine(powerRate);
