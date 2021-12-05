3504ca69748941097a090bf62869b582f312b3d5
SamA21 <Samaallen@gmail.com>
2021-12-05 13:11:50 +0000

    Day 5 start

---
+++ AdventOfCode2021/Day5/Program.cs
@@ -0,0 +1,9 @@
+﻿using AdventHelper;
+using Day5;
+
+AdventTextReader reader = new AdventTextReader();
+string inputLocation = $"data.txt";
+var ventLocations = reader.GetListFromFile(inputLocation);
+Vents vents = new Vents();
+var overlapCount = vents.VentsOverlap(ventLocations);
+Console.WriteLine($"{overlapCount} vents overlap");
---
+++ AdventOfCode2021/Day5/Vents.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    public class Vents
+    {
+        List<int[,]> Points { get; set; }
+        public Vents()
+        {
+            Points = new List<int[,]>();
+        }
+
+        public int VentsOverlap(List<string> data)
+        {
+            int overlapCount = 0;
+            foreach(var vents in data)
+            {
+                var startEnd = vents.Split("->", StringSplitOptions.RemoveEmptyEntries);
+                var intValues = GetXYValues(startEnd);
+                int XDiff = intValues.startX - intValues.endX;
+                int YDiff = intValues.startY - intValues.endY;
+            }
+            return overlapCount;
+        }
+
+        private (int startX, int startY, int endX, int endY) GetXYValues(string[] startEnd)
+        {
+            var start = startEnd[0];
+            var end = startEnd[1];
+            var startSplit = GetIntValues(start);
+            var endSplit = GetIntValues(end);
+            return (startSplit.X, startSplit.Y, endSplit.X, endSplit.Y);
+        }
+
+        private  (int X, int Y) GetIntValues(string side)
+        {
+            var split = side.Split(',', StringSplitOptions.TrimEntries);
+            int X = int.Parse(split[0]);
+            int Y = int.Parse(split[1]);
+            return (X, Y);
+        }
+    }
+}
