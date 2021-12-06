d4af1535608716d5e788c40346407cf50c75b481
SamA21 <Samaallen@gmail.com>
2021-12-06 12:57:19 +0000

    Day 6 part 2 - done

--- AdventOfCode2021/Day6/LanternFish.cs
+++ AdventOfCode2021/Day6/LanternFish.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,6 +29,51 @@ namespace Day6
                 days--;
             }
             return initalFish;
+        }
+        
+        /// <summary>
+        /// As 256 days used to much memory due to expontial growth this method was created 
+        /// </summary>
+        /// <param name="days"></param>
+        /// <param name="initalFish"></param>
+        /// <returns></returns>
+        public long SpawnFishUnlimited(int days, List<int> initalFish)
+        {
+            long[] fishArray = SetupFishArray(initalFish);
+
+            while (days > 0)
+            {
+                fishArray = SpawnFish(fishArray);
+                days--;
+            }
+
+            return fishArray.Sum();
+        }
+
+        private long[] SetupFishArray(List<int> initalFish)
+        {
+            long[] fishArray = new long[9];
+            foreach (int fish in initalFish)
+            {
+                fishArray[fish]++;
+            }
+            return fishArray;
+        }
+
+        private long[] SpawnFish(long[] fishArray)
+        {
+            long zeroFish = 0;
+            for (int index = 0; index < fishArray.Length; index++)
+            {
+                if (index == 0)
+                    zeroFish = fishArray[index];
+                else
+                    fishArray[index - 1] += fishArray[index];
+                fishArray[index] = 0;
+            }
+            fishArray[6] += zeroFish;
+            fishArray[8] = zeroFish;
+            return fishArray;
         }
     }
 }
--- AdventOfCode2021/Day6/Program.cs
+++ AdventOfCode2021/Day6/Program.cs
@@ -6,4 +6,7 @@ string inputLocation = $"data.txt";
 var laternFish = reader.GetSingleIntListFromFile(inputLocation);
 LanternFish lf = new LanternFish();
 var spawnedFish = lf.SpawnFish(80, laternFish);
-Console.WriteLine($"Spawned {spawnedFish.Count}");
+Console.WriteLine($"Spawned {spawnedFish.Count}"); 
+laternFish = reader.GetSingleIntListFromFile(inputLocation);
+var spawnedFishUnlimited = lf.SpawnFishUnlimited(256, laternFish);
+Console.WriteLine($"Spawned {spawnedFishUnlimited}");
