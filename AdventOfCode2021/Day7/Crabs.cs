028ce024f310e44f5e128866e0055c6a91aeff46
SamA21 <Samaallen@gmail.com>
2021-12-07 07:38:54 +0000

    Day 7 - part 1 done

---
+++ AdventOfCode2021/Day7/Crabs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    public class Crabs
+    {
+        public int MoveCrabs(List<int> crabPositions)
+        {
+            int maxValue = crabPositions.Max();
+            List<int> totalFuel = new List<int>();
+            for(int target = 0; target < maxValue; target++)
+            {
+                int currentFuel = GetFuelUsage(target, crabPositions);
+                totalFuel.Add(currentFuel);
+            }
+            
+            return totalFuel.Min();
+        }
+
+        private int GetFuelUsage(int target, List<int> crabPositions)
+        {
+            int totalFuel = 0;
+            foreach(int position in crabPositions)
+            {
+                int fuel = position - target;
+                if (fuel < 0)
+                    fuel = fuel * -1;
+                totalFuel += fuel;
+            }
+            return totalFuel;
+        }
+    }
+}
---
+++ AdventOfCode2021/Day7/Program.cs
@@ -0,0 +1,9 @@
+﻿using AdventHelper;
+using Day7;
+
+AdventTextReader reader = new AdventTextReader();
+string inputLocation = $"data.txt";
+var crabSubs = reader.GetSingleIntListFromFile(inputLocation);
+Crabs crabs = new Crabs();
+var fuelUsed = crabs.MoveCrabs(crabSubs);
+Console.WriteLine($"Used {fuelUsed}");
