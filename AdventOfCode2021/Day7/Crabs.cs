a7407d8105de9102bc5c0abe84a024064935b582
SamA21 <Samaallen@gmail.com>
2021-12-07 07:49:33 +0000

    Day 7 - part 2 done

--- AdventOfCode2021/Day7/Crabs.cs
+++ AdventOfCode2021/Day7/Crabs.cs
@@ -14,24 +14,59 @@ namespace Day7
             List<int> totalFuel = new List<int>();
             for(int target = 0; target < maxValue; target++)
             {
-                int currentFuel = GetFuelUsage(target, crabPositions);
+                int currentFuel = GetFuelUsage(target, crabPositions, false);
                 totalFuel.Add(currentFuel);
             }
             
             return totalFuel.Min();
         }
 
-        private int GetFuelUsage(int target, List<int> crabPositions)
+        private int GetFuelUsage(int target, List<int> crabPositions, bool isAcutal)
         {
             int totalFuel = 0;
             foreach(int position in crabPositions)
             {
-                int fuel = position - target;
-                if (fuel < 0)
-                    fuel = fuel * -1;
-                totalFuel += fuel;
+                if (!isAcutal)
+                    totalFuel += FuelUsuage(position, target);
+                else
+                    totalFuel += FuelUsuageActual(position, target);
             }
             return totalFuel;
+        }
+
+        private int FuelUsuage(int position, int target)
+        {
+            int fuel = position - target;
+            if (fuel < 0)
+                fuel = fuel * -1;
+            return fuel;
+        }
+        private int FuelUsuageActual(int position, int target)
+        {
+            int steps = position - target;
+            if(steps < 0) 
+                steps = steps * -1;
+            int fuelStep = 1;
+            int fuel = 0;
+            while(steps > 0)
+            {
+                fuel = fuel + fuelStep;
+                fuelStep++;
+                steps--;
+            }
+            return fuel;
+        }
+        public int MoveCrabsActual(List<int> crabPositions)
+        {
+            int maxValue = crabPositions.Max();
+            List<int> totalFuel = new List<int>();
+            for (int target = 0; target < maxValue; target++)
+            {
+                int currentFuel = GetFuelUsage(target, crabPositions, true);
+                totalFuel.Add(currentFuel);
+            }
+
+            return totalFuel.Min();
         }
     }
 }
--- AdventOfCode2021/Day7/Program.cs
+++ AdventOfCode2021/Day7/Program.cs
@@ -7,3 +7,6 @@ var crabSubs = reader.GetSingleIntListFromFile(inputLocation);
 Crabs crabs = new Crabs();
 var fuelUsed = crabs.MoveCrabs(crabSubs);
 Console.WriteLine($"Used {fuelUsed}");
+crabSubs = reader.GetSingleIntListFromFile(inputLocation);
+fuelUsed = crabs.MoveCrabsActual(crabSubs);
+Console.WriteLine($"Used with actual fuel rate {fuelUsed}");
