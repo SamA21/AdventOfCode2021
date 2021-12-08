e884235301d73a2e078ca8803e8b30127b077592
SamA21 <Samaallen@gmail.com>
2021-12-08 15:41:21 +0000

    Day 8 - part 2 done

--- AdventOfCode2021/Day8/Program.cs
+++ AdventOfCode2021/Day8/Program.cs
@@ -6,4 +6,6 @@ string inputLocation = $"data.txt";
 var segmentSignals = reader.GetListFromFile(inputLocation);
 Segments segments = new Segments(segmentSignals);
 var unquieNumbers = segments.FindeUnquieNumbers(true);
-Console.WriteLine($"There are {unquieNumbers}");
+Console.WriteLine($"There are {unquieNumbers} unquie numbers");
+var codedTotal = segments.FindeUnquieNumbers(false);
+Console.WriteLine($"The coded total is {codedTotal}");
--- AdventOfCode2021/Day8/Segments.cs
+++ AdventOfCode2021/Day8/Segments.cs
@@ -10,6 +10,7 @@ namespace Day8
     {
         private List<Tuple<string, string>> CodedNumbers { get; set; }
         private Dictionary<string, int> Values { get; set; }
+        private Dictionary<string, int> Values2 { get; set; }
 
 
         public Segments(List<string> input)
@@ -31,7 +32,6 @@ namespace Day8
             Values.Add("acf", 7);
             Values.Add("abcdefg", 8);
             Values.Add("abcdfg", 9);
-
         }
 
         public int FindeUnquieNumbers(bool easyNumbersOnly)
@@ -42,7 +42,7 @@ namespace Day8
             }
             else
             {
-                return FindUnquieAllNumbers();
+                return FindTotalOfCodedNumbers();
             }
 
 
@@ -104,9 +104,124 @@ namespace Day8
             return foundNumbers;
         }
 
-        private int FindUnquieAllNumbers()
+        private int FindTotalOfCodedNumbers()
         {
-            return 0;
+            int totalCoded = 0;
+            foreach (var item in CodedNumbers)
+            {
+                var values = SetupValues(item.Item1);
+                var splitItem = item.Item2.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string textNumber = "";
+                foreach(var code in splitItem)
+                {
+                    int? number = FindNumber(code, values);
+                    if (number != null)
+                        textNumber = $"{textNumber}{number}";
+                    else
+                        throw new Exception("Shouldn't be here");
+                }
+                int parsedNumber = int.Parse(textNumber);
+                totalCoded += parsedNumber;
+            }
+            return totalCoded;
         }
+
+        private Dictionary<string, int> SetupValues(string codedValues)
+        {
+            var splitCodes = codedValues.Split(' ',StringSplitOptions.RemoveEmptyEntries); 
+            Dictionary<string, int> values = new Dictionary<string,int>();
+            values = SetupEasyNumbers(splitCodes, values);
+            values = SetupOtherNumbers(splitCodes, values);
+            return values;
+        }
+
+        private Dictionary<string, int> SetupEasyNumbers(string[] splitCodes, Dictionary<string, int> values)
+        {
+            foreach(var splitCode in splitCodes)
+            {
+                switch (splitCode.Length)
+                {
+                    case 2:
+                        values.Add(string.Join("", splitCode.OrderBy(x => x)), 1);
+                        break;
+                    case 3:
+                        values.Add(string.Join("", splitCode.OrderBy(x => x)), 7);
+                        break;
+                    case 4:
+                        values.Add(string.Join("", splitCode.OrderBy(x => x)), 4);
+                        break;
+                    case 7:
+                        values.Add(string.Join("", splitCode.OrderBy(x => x)), 8);
+                        break;
+                    default:
+                        //do nothing as not easy number
+                        break;
+                }
+            }
+            return values;
+        }
+
+        private Dictionary<string, int> SetupOtherNumbers(string[] splitCodes, Dictionary<string, int> values)
+        {
+            //contains 2, 3 and 5
+            var lenghth5 = splitCodes.Where(x => x.Length == 5).ToList();
+            //contains 0, 6 and 9
+            var lenghth6 = splitCodes.Where(x => x.Length == 6).ToList();
+
+            //3 contains all of 1
+            values = FindValue(ref lenghth5, values, 1, 3);
+            //9 contains all of 3
+            values = FindValue(ref lenghth6, values, 3, 9);        
+            //0 contains all of 9
+            values = FindValue(ref lenghth6, values, 1, 0);
+            if(lenghth6.Count == 1)          
+                values.Add(string.Join("", lenghth6[0].OrderBy(x => x)), 6);
+            //5 contains all of 6 otherwise is 2
+            values = FindLast2(ref lenghth5, values, 6, 5,2);
+
+            return values;
+        }
+
+        private Dictionary<string, int> FindLast2(ref List<string> splitCodes, Dictionary<string, int> values, int searchNumber, int findingNumber1, int findingNumber2)
+        {
+            var searchValue = values.First(y => y.Value == searchNumber).Key;
+            foreach(var code in splitCodes)
+            {
+                bool matched = code.All(searchValue.Contains);
+                if (matched)
+                    values.Add(string.Join("",code.OrderBy(x=>x)), findingNumber1);
+                else
+                    values.Add(string.Join("", code.OrderBy(x => x)), findingNumber2);
+            }
+            return values;
+        }
+
+        private Dictionary<string, int> FindValue(ref List<string> splitCodes, Dictionary<string, int> values, int searchNumber, int findingNumber)
+        {
+            var searchValue = values.First(y => y.Value == searchNumber).Key;
+            var value = splitCodes.First(x => searchValue.All(x.Contains));
+            values.Add(string.Join("", value.OrderBy(x => x)), findingNumber);
+            splitCodes.Remove(value);
+            return values;
+        }
+
+        private int? FindNumber(string codedNumber, Dictionary<string, int> values)
+        {
+            var numbers = values.Where(x => x.Key.Length == codedNumber.Length).OrderBy(x => x.Value);
+
+            if(numbers.Count() == 1)
+                return numbers.First().Value;
+            else
+            {
+                codedNumber = string.Join("", codedNumber.OrderBy(x => x));
+                foreach(var number in numbers)
+                {
+                    if(number.Key == codedNumber)
+                        return number.Value;
+                }
+                return null;
+            }
+        }
+
     }
 }
